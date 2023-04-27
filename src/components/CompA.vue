f524a48d7809931ff4dbc86f39a616292763f66e
Jungzl <13jungzl@gmail.com>
2023-04-27 09:47:52 +0800

    chore: init

---
+++ src/components/CompA.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  defineOptions({
+    name: 'CompA',
+  })
+
+  const foo = ref('bar')
+
+  defineSlots<{
+    default: {
+      foo: string,
+      buz: number
+    }
+  }>()
+</script>
+
+<template>
+  <div>
+    <slot v-bind="{ foo }" />
+    <!-- ^^^ Property 'slot' does not exist on type 'Pick<IntrinsicElements, "div">'. -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/CompB.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  import CompA from './CompA.vue'; // using `unplugin-vue-components` and comment this line will throw error below
+
+  defineOptions({
+    name: 'CompB',
+  })
+</script>
+
+<template>
+  <CompA>
+    <!-- ^^^ Property 'CompA' does not exist on type 'Pick<IntrinsicElements, "template"> & Pick<IntrinsicElements, "div">'. -->
+    <template #default="{ foo, buz }">
+      <div>
+        {{ foo }}{{ buz }}
+      </div>
+    </template>
+  </CompA>
+</template>
+
+<style lang="scss" scoped></style>
